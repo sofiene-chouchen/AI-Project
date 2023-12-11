20c0f07f650016d99d1aa74e9984c222cd99c140
sofiene-chouchen <chouchene.sofiene@outlook.com>
2023-12-11 10:19:20 +0100

    add somme feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
 import { useState } from 'react';
 import './index.css';
 import Select from 'react-select';
-
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
-  { value: 'tomato', label: 'tomato' },
-];
-
+import options from './ingredients.json';
 
 function App() {
-  const [data , setData] = useState('');
+  const [data, setData] = useState({});
   console.log(data);
-  
+
+  const onChange = (e: object) => {
+    setData(e);
+  };
+
+  const sendData = () => {
+    console.log(data);
+  };
   return (
     <div className=''>
       <div className='container m-auto mt-36 '>
@@ -25,11 +25,14 @@ function App() {
             placeholder='select  your ingredients'
             className='basic-multi-select text-center'
             classNamePrefix='select'
-            
+            onChange={onChange}
           />
         </div>
         <div className='text-center'>
-          <button className='text-white rounded-md px-4 py-1 bg-blue-600 mt-3'>
+          <button
+            className='text-white rounded-md px-4 py-1 bg-blue-600 mt-3'
+            onClick={sendData}
+          >
             Recherche
           </button>
         </div>
