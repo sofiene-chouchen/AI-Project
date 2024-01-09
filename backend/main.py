a3bcd4a8338ca43a94f38107b6783810fa107368
sofiene-chouchen <chouchene.sofiene@outlook.com>
2024-01-09 19:55:56 +0100

    integration

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,11 @@ from tensorflow.keras.models import load_model
 from keras.preprocessing.text import tokenizer_from_json
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import json
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
+
+
 
 # Load the model and tokenizer
 model = load_model("recipe_model.h5")
@@ -28,15 +33,21 @@ except FileNotFoundError:
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
-def get():
-  # Example of using the loaded model and tokenizer for prediction
-  new_ingredients = "pecan vanilla water"
-  new_ingredients_sequence = tokenizer.texts_to_sequences([new_ingredients])
-  new_ingredients_sequence_padded = pad_sequences(new_ingredients_sequence, maxlen=90)
-  predicted_title_index = int(model.predict(new_ingredients_sequence_padded).argmax())
-  print(predicted_title_index)
-# Decode the predicted index back to title
-  predicted_title = titles_list[predicted_title_index]
-  print(predicted_title)
-  return{"message": predicted_title}
+def get(ingredients: str):
+    # Example of using the loaded model and tokenizer for prediction
+    new_ingredients_sequence = tokenizer.texts_to_sequences([ingredients])
+    new_ingredients_sequence_padded = pad_sequences(new_ingredients_sequence, maxlen=90)
+    predicted_title_index = int(model.predict(new_ingredients_sequence_padded).argmax())
+    
+    # Decode the predicted index back to title
+    predicted_title = titles_list[predicted_title_index]
+    
+    return {"message": predicted_title}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,45 +2,66 @@ import { useState } from 'react';
 import './index.css';
 import Select from 'react-select';
 import options from './ingredients.json';
+import axios from 'axios';
 
 function App() {
   const [data, setData] = useState({});
+  const [title, setTitle] = useState('');
 
-  const onChange = (e: object[]) => {
-    const values = e.map((item) => item.value);
-    setData(values);
+  const onChange = (e: [string]) => {
+    const values = e.map((item: string) => item.value);
+    const joinedValues = values.join(' ');
+    setData(joinedValues);
   };
 
   const sendData = () => {
-    console.log(data);
+    axios
+      .get(`http://localhost:8000/?ingredients=${data}`)
+      .then((response) => {
+        setTitle(response.data.message);
+      })
+      .catch((error) => {
+        console.error('Error fetching data from backend:', error);
+      });
   };
-  return (
-    <div className=''>
-      <div className='container m-auto mt-36 '>
-        <div className=''>
-          <Select
-            isMulti
-            name='colors'
-            options={options}
-            placeholder='select  your ingredients'
-            className='basic-multi-select text-center'
-            classNamePrefix='select'
-            onChange={onChange}
-          />
-        </div>
-        <div className='text-center'>
-          <button
-            className='text-white rounded-md px-4 py-1 bg-blue-600 mt-3'
-            onClick={sendData}
-          >
-            Recherche
-          </button>
-        </div>
 
-        <div className='bg-slate-600 w-44 h-28 mt-20 rounded-md '>
-          <p className='content-center'>title</p>
-        </div>
+  return (
+    <div className='container m-auto mt-8'>
+      <h1 className='text-4xl font-bold mb-4 text-center mt-20'>
+        Your Project Title
+      </h1>
+
+      <p className='text-lg mb-4 mt-10'>
+        Choose your ingredients and click "Search" to see the result.
+      </p>
+
+      <div className='text-center'>
+        <Select
+          isMulti
+          name='colors'
+          options={options}
+          placeholder='Select your ingredients'
+          className='basic-multi-select text-center'
+          classNamePrefix='select'
+          onChange={onChange}
+        />
       </div>
+
+      <div className='text-center mt-4'>
+        <button
+          className='text-white rounded-md px-4 py-1 bg-blue-600'
+          onClick={sendData}
+        >
+          Search
+        </button>
+      </div>
+
+      {title && (
+        <div className='bg-gray-300 bg-opacity-80 w-64 h-auto mt-8 rounded-md mx-auto p-4 flex flex-col items-center'>
+          <h2 className='text-black text-lg font-bold mb-2'>Result:</h2>
+          <p className='text-black text-center'>{title}</p>
+        </div>
+      )}
     </div>
   );
 }
